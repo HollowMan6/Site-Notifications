40e57fef5b7d23722484759d34e3871a0acbcdd3
Hollow Man <hollowman@hollowman.ml>
2021-02-26 18:56:17 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,6 @@
 'use strict';
 
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
-
 module.exports = {
   mode: "development",
-  resolve: {
-    fallback: {
-      net: false,
-      tls: false,
-      fs: false,
-    }
-  },
-  plugins: [
-    new NodePolyfillPlugin()
-  ]
+  target: 'node'
 }
